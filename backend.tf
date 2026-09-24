4f8e089c8d044e53df0ebd88f4aed7c423fa6386
BalaKrishna <baladevops2023@gmail.com>
2026-09-24 10:01:41 +0530

    backend updated state to store in s3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "aws-s3-22-sep-mindtech"
-    key    = "dev-env"
+    key    = "dev-env/terraform.tfstate"
     region = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
   }
 }
